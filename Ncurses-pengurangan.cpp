d0d25bb3b4666cc34205d2b2e527ce600a9c7923
Ikhsan saputra <92314859+Ikhssptr25@users.noreply.github.com>
2021-12-18 21:21:14 +0700

    Add files via upload

---
+++ Ncurses-pengurangan.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <ncurses/ncurses.h>
+#include<windows.h>
+using namespace std;
+
+/*ikhsan saputra
+Npm:2117051037
+kelas:B*/
+
+int main(){
+	
+	
+	//variabel//
+	int a;
+	int b;
+	int c;
+	
+	//input//
+	cout<<"Masukkan angka : "; 
+	cin>>b;
+	cout<<"Masukkan angka : "; 
+	cin>>c;
+	
+	
+	a=b-c;
+	initscr();
+		for(int i=1;i<=3;i++){
+		printw("Loading");
+		Sleep(250);
+		refresh();
+		clear();
+		printw("Loading..");
+		Sleep(250);
+		refresh();
+		clear();
+		printw("Loading...");
+		Sleep(250);
+		refresh();
+		clear();
+		printw("Loading....");
+		Sleep(250);
+		refresh();
+		clear();
+	}
+	
+	//output//
+	cout<<"Hasil pengurangan  :"<< a <<endl;
+	
+
+	refresh();
+	getch();
+	
+	endwin();
+}
